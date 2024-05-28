9f844a37cd5b52f039c368c70ffa8a8d7fe886e0
PierreBarge <pierrebarge@hotmail.fr>
2024-05-28 16:06:57 +0200

    Harmonia quest 03: Routing

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
+// const itemsRouter = require("./items/router");
 
-router.use("/items", itemsRouter);
+// router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
